a1341697aad5ea4f3bae5d256ae1f7846a6e148d
Naufal-Azka <askanaufal74@gmail.com>
2025-04-30 04:04:12 +0700

    Refactor navbar scroll behavior: update backdrop filter and text color based on scroll position

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ let isNavOpen = false;
 
 // Function to make the navbar change backdrop filter when scrolled for certain offset
 const changeNavbarOnScroll = () => {
-  const scrollOffset = window.scrollY;
-  if (scrollOffset > 100) {
-    navbar.classList.remove('backdrop-blur-base');
-    navbar.classList.remove('bg-white/20');
+    const scrollOffset = window.scrollY;
+    if (scrollOffset > 100) {
+        navbar.classList.remove('text-white');
 
-    navbar.classList.add('bg-white');
-    navbarItem.classList.add('border-b-2');
-  } else {
-    
-    navbar.classList.add('backdrop-blur-base');
-    navbar.classList.add('bg-white/20');
+        navbar.classList.add('bg-white');
+        navbarItem.classList.add('border-b-2');
+    } else {
+        navbar.classList.remove('bg-white');
+        navbarItem.classList.remove('border-b-2');
 
-    navbar.classList.remove('bg-white');
-    navbarItem.classList.remove('border-b-2');
-  }
+        navbar.classList.add('text-white')
+    }
 }
 
 document.addEventListener("scroll", changeNavbarOnScroll);
